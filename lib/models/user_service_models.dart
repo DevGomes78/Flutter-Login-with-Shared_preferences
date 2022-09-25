16c5ec3ea8c11ba4610bbcd3f8d9d046d4817f96
usuario <gomes.amilson@gmail.com>
2022-09-25 02:40:48 -0300

    criado compos repita senha e sobrenome no arquivo register_page.dart

--- lib/models/user_service_models.dart
+++ lib/models/user_service_models.dart
@@ -1,27 +1,46 @@
 class UserModel {
   String? name;
+  String? sobrenome;
   String? mail;
   String? senha;
+  String? repitasenha;
 
-  UserModel({this.name, this.mail, this.senha});
+  UserModel({
+    this.name,
+    this.sobrenome,
+    this.mail,
+    this.senha,
+    this.repitasenha,
+  });
 
   UserModel.fromJson(Map<String, dynamic> json) {
     name = json['name'];
+    sobrenome = json['sobrenome'];
     mail = json['mail'];
     senha = json['senha'];
+    repitasenha = json['repitasenha'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = Map<String, dynamic>();
     data['name'] = name;
+    data['sobrenome'] = sobrenome;
     data['mail'] = mail;
     data['senha'] = senha;
+    data['repitasenha'] = repitasenha;
     return data;
   }
-  String toString(){
-    return "Name: "+ this.name.toString() +
-        "\nE-mail :" +this.mail.toString()+
-         "\nSenha :" + this.senha.toString();
 
+  String toString() {
+    return "Name: " +
+        this.name.toString() +
+        "\nsobrenome: "+
+        this.sobrenome.toString()+
+        "\nE-mail :" +
+        this.mail.toString() +
+        "\nSenha :" +
+        this.senha.toString()+
+       "\nrepitasenha: " +
+        this.repitasenha.toString();
   }
 }
