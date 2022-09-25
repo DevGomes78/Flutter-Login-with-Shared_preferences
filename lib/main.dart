001f220f033c12bd2bd691da314fe98589d5a773
usuario <gomes.amilson@gmail.com>
2022-09-25 02:00:51 -0300

    alterado nome do arquivo splash_page.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_logar_listar/views/splash.dart';
+import 'package:flutter_logar_listar/views/splash_page.dart';
 
 void main() =>  runApp(const MyApp());
 
