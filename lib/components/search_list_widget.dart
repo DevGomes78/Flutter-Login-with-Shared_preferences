18bb8fd7563b56ccb421ea018c5a27806f64d760
usuario <gomes.amilson@gmail.com>
2022-09-27 04:03:41 -0300

    atualizado layout splash_page.dart

--- lib/components/search_list_widget.dart
+++ lib/components/search_list_widget.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_logar_listar/constants/string_constants_login.dart';
-import 'package:flutter_logar_listar/service/user_api_controller.dart';
+import 'package:flutter_logar_listar/service/user_api_service.dart';
 import 'package:flutter_logar_listar/models/user_api_models.dart';
 
 import '../controlers/call_github_repository.dart';
--- lib/views/user_page.dart
+++ lib/views/user_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_logar_listar/controlers/call_github_repository.dart';
 import 'package:flutter_logar_listar/components/search_list_widget.dart';
-import 'package:flutter_logar_listar/service/user_api_controller.dart';
+import 'package:flutter_logar_listar/service/user_api_service.dart';
 import 'package:flutter_logar_listar/models/user_api_models.dart';
 import 'package:flutter_logar_listar/views/shimmer_page.dart';
 import '../components/drawer_widget.dart';
